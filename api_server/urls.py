1a0428e9617726062fbdbaadc7b921b0bc428d22
Biswajit <patrabiswajit133@gmail.com>
2020-12-21 21:59:19 +0530

    Add new api call

--- api_server/urls.py
+++ api_server/urls.py
@@ -7,6 +7,4 @@ router.register(r'notes', views.NoteViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
 ]
--- api_server/views.py
+++ api_server/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .serializers import NoteSerializer
 from .models import Note
@@ -8,12 +9,23 @@ class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
+    @action(methods=['GET'], name='Get Titles', detail=False)
+    def list_titles(self,request):
+        """Custom action for returing only titles
+
+        Returns:
+            List of titles
+        """       
+        notes = self.get_queryset()
+        serializer = self.get_serializer_class()(notes, fields={'title'},many=True)
+        return Response(serializer.data) 
+
     def list(self, request):
         """Overrides LIST request
-        
+
         Returns:
-            All the titles of Notes
+            All the titles of Notes with ids hiding the content
         """        
         notes = self.get_queryset()
-        serializer = self.get_serializer_class()(notes, many=True)
+        serializer = self.get_serializer_class()(notes, fields={'id','title'},many=True)
         return Response(serializer.data)
